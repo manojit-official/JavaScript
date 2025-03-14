3b06923f7fb841dc83dedc487084c0aa58b7df70
Manojit Biswas <manojit.biswas.officialwork@gmail.com>
2025-03-14 17:24:06 +0530

    js datatypes

---
+++ JS Data Types/datatypes.js
@@ -0,0 +1,18 @@
+let a = 10; //number
+let b = "hello"; //string
+let c = true; //boolean
+let d = null; //null
+let e = undefined; //undefined
+let salary = BigInt(100000); //bigint  you want to store a big number/integer
+let value = Symbol("id"); //symbol want to store some unique values in javascript you can use symbol and the data type of this value will become as symbol
+const user = {name: "John", age: 30}; //object  where you are defining the key value pairs thats is called user object
+console.log(typeof(e)); //what type of data is stored in variable you can also know with the help of this type of keyword
+
+//Questions1- what does starting a variable name with an underscore ,like _myvar,indicate?
+//ANS- it's a convention for private variables.
+
+//Questions2- what is the purpose of using camelCase when naming variables in javascript?
+//ANS- Naming variables for readability.
+
+//Questions3- you need to create an empty object to store user profile data.How can you define this?
+//ANS- var user = {};
